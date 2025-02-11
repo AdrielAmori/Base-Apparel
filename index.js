b651ca8bedc9eca8bb04670ad4d20ead8189c238
AdrielAmori <adriel680@gmail.com>
2025-02-11 13:54:53 -0300

    update index. tml, style.css and index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var emailInput = document.getElementById('emailInput');
+  var resultMessage = document.getElementById('resultMessage');
+  var emailForm = document.getElementById('emailForm');
+  var errorIcon = document.getElementById('errorIcon');
+
+  emailForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = emailInput.value;
+
+    if (validateEmail(email)) {
+      resultMessage.textContent = "Email válido";
+      resultMessage.style.color = "green";
+      errorIcon.style.display = "none";
+    } else {
+      resultMessage.textContent = "Email inválido";
+      resultMessage.style.color = "red";
+      errorIcon.style.display = "block";
+    }
+  });
+
+  // Função de validar email usando regex
+  function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+});
